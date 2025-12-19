00a2576747cbcad8e97f9d600ada3b97ba13067b
Yash Bhavsar_1020 <154401282+226240307005@users.noreply.github.com>
2025-12-19 15:04:31 +0530

    Implement winner popup and reset functionality
    Added popup functionality to display the winner and reset game state.

--- app.js
+++ app.js
@@ -1,86 +1,108 @@
-let boxes=document.querySelectorAll(".box")
-let playBtn= document.getElementById("playBtn");
-let winner=document.getElementById("winner")
-let reset=document.getElementById("resetBtn")
-let turnO=true
-
-let winPos=[
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,4,8],
-    [2,4,6],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-
+let boxes = document.querySelectorAll(".box")
+let playBtn = document.getElementById("playBtn");
+let reset = document.getElementById("resetBtn")
+let closeBtn=document.getElementById("closeBtn")
+ let popup=document.getElementById("popup");
+  let text=document.getElementById("winner");
+let turnO = true
+let i=0;
+let winPos = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
 ]
-reset.addEventListener("click",()=>{
- for(let box of boxes){
-      turnO=true
-    box.innerText=""
-     box.disabled=false
-  }
-})
-playBtn.addEventListener("click",()=>{
-  turnO=true
-  playBtn.hidden=true
- winner.hidden=true
- winner.innerText=""
-  for(let box of boxes){
-    box.disabled=false
-    box.innerText=""
-  }
 
+reset.addEventListener("click", () => {
+  i=0;
+  turnO = true
+  playBtn.hidden = true
+
+  for (let box of boxes) {
+    box.innerText = ""
+    box.disabled = false
+  }
 })
 
-boxes.forEach((box)=>{
-    box.addEventListener("click",()=>{
-      
-        if(turnO){
-            box.innerText="O"
-            turnO=false
-        }
-        else{
-            box.innerText="X"
-            turnO=true
-        }
-     
-        box.disabled=true
-        checkWinner()
-    })
-})
-let disabledBox=()=>{
-  for(let box of boxes){
-    box.disabled=true
+playBtn.addEventListener("click", () => {
+  i=0
+  turnO = true
+  playBtn.hidden = true
+ 
+
+  for (let box of boxes) {
+    box.disabled = false
+    box.innerText = ""
   }
- playBtn.hidden=false;
+})
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O"
+      turnO = false
+    } else {
+      box.innerText = "X"
+      turnO = true
+    }
+
+    box.disabled = true
+    checkWinner()
+  })
+})
+
+let disabledBox = () => {
+  for (let box of boxes) {
+    box.disabled = true
+  }
+  
+  playBtn.hidden = false
 }
-const checkWinner=()=>{
-winner.hidden=false
-    for(let pos of winPos){
-       
-         //console.log( pos[0],pos[1],pos[2])
-       // console.log(boxes[pos[0]],boxes[pos[1]],boxes[pos[2]])
-        if(boxes[pos[0]].innerText!="" && boxes[pos[1]].innerText!="" && boxes[pos[2]].innerText!=""){
-       if(boxes[pos[0]].innerText=="X" && boxes[pos[1]].innerText=="X" && boxes[pos[2]].innerText=="X"){
-            console.log("X win")
-           winner.innerText="X win!!"
-          disabledBox()
-            
-            
-       }
-       else if(boxes[pos[0]].innerText=="O" && boxes[pos[1]].innerText=="O" && boxes[pos[2]].innerText=="O"){
-            console.log("O win")
-          winner.innerText="O win!!"
-           disabledBox()
-           
-       }
+
+function popUp(win){
+  popup.style.display="flex"
+  text.innerText=win
+  
+}
+closeBtn.addEventListener("click",()=>{
+   popup.style.display="none";
+})
+
+const checkWinner = () => {
      
-   
+  for (let pos of winPos) {
+
+    if (
+      boxes[pos[0]].innerText !== "" &&
+      boxes[pos[1]].innerText !== "" &&
+      boxes[pos[2]].innerText !== ""
+    ) {
+
+      if (
+        boxes[pos[0]].innerText === "X" &&
+        boxes[pos[1]].innerText === "X" &&
+        boxes[pos[2]].innerText === "X"
+      ) {
+       let win= "The winner is X!"
+        popUp(win)
+        disabledBox()
+        return
+      }
+
+      if (
+        boxes[pos[0]].innerText === "O" &&
+        boxes[pos[1]].innerText === "O" &&
+        boxes[pos[2]].innerText === "O"
+      ) {
+       let win= "The winner is O!"
+        popUp(win)
+        disabledBox()
+        return
+      }
     }
-       
-    }
-    
-   
+  }
 }
